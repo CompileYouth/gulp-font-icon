6c376f0d0344436a5e337c2da8e230e48cef7194
CompileYouth <CompileYouth@163.com>
2016-05-04 15:56:36 +0800

    modify index.js and package.json

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use strict";
 
-var gutil = require("gulp-util");
-var PluginError = gutil.PluginError;
-var through = require("through2");
+var duplexer = require('plexer');
+var svgicons2svgfont = require('gulp-svgicons2svgfont');
+var svg2ttf = require('gulp-svg2ttf');
+var ttf2eot = require('gulp-ttf2eot');
+var ttf2woff = require('gulp-ttf2woff');
+var ttf2woff2 = require('gulp-ttf2woff2');
 
 const PLUGIN_NAME = "gulp-font-icon";
 
@@ -21,7 +24,23 @@ function gulpFontIcon(options) {
 		options.formats = ["ttf", "eot", "woff", "woff2", "svg"];
 	}
 
-	
+	var inStream = null;
+	var outStream = null;
+	var duplexStream = null;
+
+	options = options || {};
+
+	inStream = svgicons2svgfont(options);
+
+	outStream = inStream
+		.pipe(svg2ttf({ clone: true }))
+		.pipe(ttf2eot({ clone: true }))
+		.pipe(ttf2woff({ clone: true }))
+		.pipe(ttf2woff2({ clone: true }));
+
+	duplexStream = duplexer({ objectMode: true }, inStream, outStream);
+
+	return duplexStream;
 };
 
 module.exports = gulpFontIcon;
