b495fd437ffd9d40a809dfeca3abc96b1fb180bd
CompileYouth <CompileYouth@163.com>
2016-05-04 18:24:15 +0800

    gen index.html

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 "use strict";
 
+var iconfontCss = require("gulp-iconfont-css");
+var gutil = require("gulp-util");
 var duplexer = require('plexer');
 var svgicons2svgfont = require('gulp-svgicons2svgfont');
 var svg2ttf = require('gulp-svg2ttf');
+var through = require('through2');
 var ttf2eot = require('gulp-ttf2eot');
 var ttf2woff = require('gulp-ttf2woff');
 var ttf2woff2 = require('gulp-ttf2woff2');
+var htmlGen = require("./lib/html-gen");
 
 const PLUGIN_NAME = "gulp-font-icon";
 
 function gulpFontIcon(options) {
-	// Examine type of parameters and Set default value
-	if (typeof options !== "object" || Array.isArray(options)) {
-		options = {};
-	}
-	if (typeof options.fontFamily !== "string") {
-		options.fontFamily = "iconFont";
-	}
-	if (typeof options.fontAlias !== "string") {
-		options.fontAlias = "if";
-	}
-	if (!Array.isArray(options.formats)) {
-		options.formats = ["ttf", "eot", "woff", "woff2", "svg"];
-	}
+  var inStream = null;
+  var outStream = null;
+  var duplexStream = null;
 
-	var inStream = null;
-	var outStream = null;
-	var duplexStream = null;
+  options = options || {};
 
-	inStream = svgicons2svgfont(options);
+  inStream = iconfontCss({
+      fontName: options.fontName,
+      targetPath: 'icons.css',
+      fontPath: '',
+      cssClass: options.fontAlias
+    }).pipe(htmlGen(options));
 
-	outStream = inStream
-		.pipe(svg2ttf({ clone: true }))
-		.pipe(ttf2eot({ clone: true }))
-		.pipe(ttf2woff({ clone: true }))
-		.pipe(ttf2woff2({ clone: true }));
+  outStream = inStream
+  	.pipe(svgicons2svgfont(options))
+    .pipe(svg2ttf({ clone: true }))
+    .pipe(ttf2eot({ clone: true }))
+    .pipe(ttf2woff({ clone: true }))
+    .pipe(ttf2woff2({ clone: true }));
 
-	duplexStream = duplexer({ objectMode: true }, inStream, outStream);
+  duplexStream = duplexer({ objectMode: true }, inStream, outStream);
 
-	return duplexStream;
-};
+  return duplexStream;
+}
 
 module.exports = gulpFontIcon;
