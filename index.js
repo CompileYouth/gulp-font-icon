55f9ada6bf4349fb217c3af59cdd735acd4c80ea
CompileYouth <CompileYouth@163.com>
2016-05-25 16:43:48 +0800

    Fix some bugs.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 "use strict";
 
-var duplexer = require('plexer');
-var gutil = require("gulp-util");
-var htmlGen = require("./lib/html-gen");
-var iconfontCss = require("gulp-iconfont-css");
-var svg2ttf = require('gulp-svg2ttf');
-var svgicons2svgfont = require('gulp-svgicons2svgfont');
-var through = require('through2');
-var ttf2eot = require('gulp-ttf2eot');
-var ttf2woff = require('gulp-ttf2woff');
-var ttf2woff2 = require('gulp-ttf2woff2');
+const duplexer = require('plexer');
+const gutil = require("gulp-util");
+const htmlGen = require("./lib/html-gen");
+const iconfontCss = require("gulp-iconfont-css");
+const svg2ttf = require('gulp-svg2ttf');
+const svgicons2svgfont = require('gulp-svgicons2svgfont');
+const through = require('through2');
+const ttf2eot = require('gulp-ttf2eot');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
 const PLUGIN_NAME = "gulp-font-icon";
 
 module.exports = function(options) {
-	
+
 };
 function gulpFontIcon(options) {
-    var inStream = null;
-    var outStream = null;
+    let inStream = null;
+    let outStream = null;
 
     options = options || {};
 
     inStream = iconfontCss({
-        fontName: options.fontName,
-        targetPath: options.fontName + ".css",
+        fontName: options.fontFamily,
+        targetPath: options.fontFamily + ".css",
         fontPath: '',
         cssClass: options.fontAlias
     });
